8fa94544c530875ffc853e6c22164036b3d12553
Vasileios Loumanis <loumanv@gmail.com>
2017-08-26 18:26:10 +0100

    Add AppController, making initial posts request

--- Posts/Controllers/AppController.swift
+++ Posts/Controllers/AppController.swift
@@ -8,6 +8,52 @@
 
 import UIKit
 
-class AppController: NSObject {
+enum AppControllerError: LocalizedError {
+    case jsonResponseEmpty
+}
 
+class AppController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadPosts()
+    }
+    
+    func loadPosts() {
+        RequestsManager.load(url: URL(string: Urls.baseUrl + Urls.postsUrl)!) { [weak self] (data, error) in
+            if error != nil {
+                self?.showErrorMessage(title: "Request Error", message: error.debugDescription)
+                return
+            }
+            
+            guard let parsedPosts = try? self?.parsePosts(data: data), let posts = parsedPosts else {
+                self?.showErrorMessage(title: "Post Parsing Error", message: "")
+                return
+            }
+            let postsVC = PostsViewController(posts: posts)
+            postsVC.modalTransitionStyle = .crossDissolve
+            self?.present(postsVC, animated: true, completion: nil)
+        }
+    }
+
+    func parsePosts(data: Any?) throws -> [Post] {
+        guard let data = data else { throw AppControllerError.jsonResponseEmpty }
+        let posts: [Post] = {
+            if let postsArray = data as? [Any] {
+                return postsArray.flatMap { postDictionary in
+                    guard let postDictionary = postDictionary as? [String : Any] else { return nil }
+                    return try? Post(dictionary: postDictionary) }
+            }
+            return [Post]()
+        }()
+        return posts
+    }
+
+    func showErrorMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
--- Posts/Controllers/PostsViewController.swift
+++ Posts/Controllers/PostsViewController.swift
@@ -2,7 +2,7 @@
 //  PostsViewController.swift
 //  Posts
 //
-//  Created by Billybatigol on 26/08/2017.
+//  Created by Vasileios Loumanis on 26/08/2017.
 //  Copyright © 2017 Vasileios Loumanis. All rights reserved.
 //
 
@@ -10,26 +10,19 @@ import UIKit
 
 class PostsViewController: UIViewController {
 
+    var posts: [Post]
+
+    init(posts: [Post]) {
+        self.posts = posts
+        super.init(nibName: String(describing: PostsViewController.self), bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
