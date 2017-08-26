cf753fc1d1d4b223d3b54c5461df73983d340dde
Billybatigol <billybatigol@BillybagolsiMac.home>
2017-08-26 14:08:20 +0100

    Add Swiftlint for consistent code styling

---
+++ Posts/Model/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Posts
+//
+//  Created by Billybatigol on 26/08/2017.
+//  Copyright © 2017 Vasileios Loumanis. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
