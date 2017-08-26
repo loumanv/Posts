7978f10f4ccef58245be97a4d696cb14714ac1a4
Vasileios Loumanis <loumanv@gmail.com>
2017-08-26 20:48:34 +0100

    Add PostCell

---
+++ Posts/Views/PostCell.swift
@@ -0,0 +1,14 @@
+//
+//  PostCell.swift
+//  Posts
+//
+//  Created by Billybatigol on 26/08/2017.
+//  Copyright © 2017 Vasileios Loumanis. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+}
